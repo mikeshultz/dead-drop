992830a44969901c35da89b9b010c9ef67dd3ecb
Mike Shultz <shultzm@gmail.com>
2023-08-21 00:45:09 -0600

    fix: bit more file watching for build.rs and outputting FE build output (for now)

--- build.rs
+++ build.rs
@@ -2,12 +2,16 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=webpack.config.js");
-    println!("cargo:rerun-if-changed=src/main.svelte");
+    println!("cargo:rerun-if-changed=src/main.ts");
+    println!("cargo:rerun-if-changed=src/components/notepad.svelte");
+    println!("cargo:rerun-if-changed=src/components/status.svelte");
     println!("cargo:warning=Building FE");
 
     // Run FE build
-    Command::new("./node_modules/.bin/webpack-cli")
+    let output = Command::new("./node_modules/.bin/webpack-cli")
         .arg("build")
         .output()
         .expect("Failed to build Svelte");
+
+    println!("cargo:warning={:?}", output);
 }
