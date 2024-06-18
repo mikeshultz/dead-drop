3bde3a6b00e311d39d332c510a803b1556d67228
Mike Shultz <shultzm@gmail.com>
2024-06-18 12:01:16 -0600

    feat: redirect to HTTPS on JS load

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ declare global {
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
+  // Ensure TLS connection if JS is enabled (required for client-side encryption)
+  if (window.location.protocol !== "https:") {
+    window.location.protocol = "https:"
+  }
+
   new Notepad({
     target: document.getElementById("notepad"), // entry point in ../public/index.html
   })
